f3cb186fe736035eec5600c2e9540799065542e5
THEXIN\28033 <280337838@qq.com>
2025-01-16 13:11:41 +0800

    添加AI聊天

--- src/main/java/org/datateam/touristassistant/service/impl/UserServiceImpl.java
+++ src/main/java/org/datateam/touristassistant/service/impl/UserServiceImpl.java
@@ -2,7 +2,10 @@ package org.datateam.touristassistant.service.impl;
 
 
 import org.datateam.touristassistant.service.UserService;
+import org.springframework.stereotype.Service;
 
+
+@Service
 public class UserServiceImpl implements UserService {
 
 }
