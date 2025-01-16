eda081ce0a4fd28814c3fc25856c34e22fab3584
THEXIN\28033 <280337838@qq.com>
2025-01-16 10:48:37 +0800

    添加加密工具

---
+++ src/main/java/org/datateam/touristassistant/utils/UUIDGenerator.java
@@ -0,0 +1,9 @@
+package com.example.disabledfitness.Utils;
+
+import java.util.UUID;
+
+public class UUIDGenerator {
+    public static UUID generateUUID() {
+        return UUID.randomUUID();
+    }
+}
