8eeb94be7200abb985e00eda188247b601bd3809
TheXin <13433647+li280337838@user.noreply.gitee.com>
2025-02-16 15:52:41 +0800

    debug

--- src/main/java/org/datateam/touristassistant/mapper/MessageMapper.java
+++ src/main/java/org/datateam/touristassistant/mapper/MessageMapper.java
@@ -3,8 +3,11 @@ package org.datateam.touristassistant.mapper;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.datateam.touristassistant.pojo.Message;
+import org.datateam.touristassistant.pojo.MessageContent;
 
 @Mapper
 public interface MessageMapper {
     void insertMessage(Message message);
+
+    void selectMessageByOpenid(String openid);
 }
